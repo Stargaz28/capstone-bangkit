c719e3a45186b5e54d1e309b96cf2b5786b43cef
Miftah Farid Maulana <miftahfrdmaulana@gmail.com>
2024-05-28 23:44:40 +0700

    Update server.js
    Make jwt hapicookie dll

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
+const HapiAuthCookie = require('@hapi/cookie');
+const db = require('./services/db');
+require('dotenv').config();
 
 const init = async () => {
     const server = Hapi.server({
@@ -12,10 +15,36 @@ const init = async () => {
         },
     });
 
+    await server.register(HapiAuthCookie);
+
+    server.auth.strategy('session', 'cookie', {
+        cookie: {
+            name: 'sid',
+            password: process.env.COOKIE_SECRET, // Ensure this is at least 32 characters long
+            isSecure: false, // Should be set to true in production
+            path: '/',
+        },
+        redirectTo: false,
+        validate: async (request, session) => {
+            try {
+                const results = await db.query('SELECT * FROM users WHERE id = ?', [session.id]);
+                const user = results[0];
+                if (!user) {
+                    return { isValid: false };
+                }
+                return { isValid: true, credentials: { user } };
+            } catch (error) {
+                return { isValid: false };
+            }
+        }
+    });
+
+    server.auth.default('session');
+
     server.route(routes);
 
     await server.start();
-    console.log(`Server: ${server.info.uri}`);
+    console.log(`Server running on ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
